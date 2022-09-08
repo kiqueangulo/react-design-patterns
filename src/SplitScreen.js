8336c5e34deb1e074bc1c4f6156d02bdcbb6ce40
kiqueangulo <kiqueangulo@gmail.com>
2022-09-08 08:53:17 -0400

    Use styled to create components

--- src/SplitScreen.js
+++ src/SplitScreen.js
@@ -1,12 +1,22 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Pane = styled.div`
+  flex: 1;
+`;
+
 export const SplitScreen = ({ left: Left, right: Right }) => {
   return (
-    <div>
-      <div>
+    <Container>
+      <Pane>
         <Left />
-      </div>
-      <div>
+      </Pane>
+      <Pane>
         <Right />
-      </div>
-    </div>
+      </Pane>
+    </Container>
   );
 };

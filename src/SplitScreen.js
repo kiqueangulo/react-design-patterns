d9c56aa2e54fbd86c1501aa9a2ec9691baf779ac
kiqueangulo <kiqueangulo@gmail.com>
2022-09-08 08:50:06 -0400

    Split-screen Create component

---
+++ src/SplitScreen.js
@@ -0,0 +1,12 @@
+export const SplitScreen = ({ left: Left, right: Right }) => {
+  return (
+    <div>
+      <div>
+        <Left />
+      </div>
+      <div>
+        <Right />
+      </div>
+    </div>
+  );
+};

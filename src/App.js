17b5ce61ecb03db78b18a0fdbc50cfc9e7e40a4b
kiqueangulo <kiqueangulo@gmail.com>
2022-09-09 16:53:48 -0400

    Initial code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import { SplitScreen } from "./SplitScreen";
-
-const LeftHandComponent = () => (
-  <h1 style={{ backgroundColor: "green" }}>Left!</h1>
-);
-
-const RightHandComponent = () => (
-  <p style={{ backgroundColor: "red" }}>Right!</p>
-);
-
 function App() {
   return (
-    <SplitScreen leftWeight={1} rightWeight={3}>
-      <LeftHandComponent />
-      <RightHandComponent />
-    </SplitScreen>
+    <>
+      <h1>App component</h1>
+    </>
   );
 }
 

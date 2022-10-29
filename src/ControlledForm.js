e9968f8b6591126c5aadc9e723e5c45915c149e2
kiqueangulo <kiqueangulo@gmail.com>
2022-10-29 03:04:12 -0400

    Controlled form
    Each field is being tracked all the time

---
+++ src/ControlledForm.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react"
+
+export const ControlledForm = () => {
+  const [name, setName] = useState("")
+  const [age, setAge] = useState(0)
+  const [hairColor, setHairColor] = useState("")
+
+  useEffect(() => {
+    if (name.length <= 2) console.log("Name must be more than two characters long.")
+  }, [name])
+
+  return (
+    <form>
+      <input
+        name="name"
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <input
+        name="age"
+        type="number"
+        placeholder="Age"
+        value={age}
+        onChange={e => setAge(Number(e.target.value))}
+      />
+      <input
+        name="hairColor"
+        type="text"
+        placeholder="Hair Color"
+        value={hairColor}
+        onChange={e => setHairColor(e.target.value)}
+      />
+
+      <button>Submit</button>
+    </form>
+  )
+}
